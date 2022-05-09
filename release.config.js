a79861f6a9a9a9b3cd134d12993d1ee7e2a30c49
Brian Lewis <brian@fourkitchens.com>
2022-05-09 07:43:30 -0500

    feat: configure releases to run against 'main'

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['YALB-430-layout-paragraphs'],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
