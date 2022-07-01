6f98e7385fa3ce33bd3b8f37bbd7085a604679ac
ModulesUnraveled <ModulesUnraveled@users.noreply.github.com>
2022-07-01 01:29:35 +0000

    build: commit compiled component library

---
+++ node_modules/@yalesites-org/component-library-twig/components/04-page-layouts/placeholder/placeholder.stories.js
@@ -0,0 +1,8 @@
+import placeHolderTwig from './placeholder.twig';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Page Layouts/Utility' };
+
+export const placeHolder = () => placeHolderTwig();
