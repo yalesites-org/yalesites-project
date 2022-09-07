b6f4a73be7050a8186ea747cdf5ac012dbc7f0c0
Brian Lewis <brian@fourkitchens.com>
2022-09-07 08:22:48 -0500

    build: colorize echos

--- scripts/local/dev-component-library-install.sh
+++ scripts/local/dev-component-library-install.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+GREEN='\033[1;32m'
+ENDCOLOR='\033[0m'
+
 read -p "Which branch? " BRANCH
 
-echo "Move into atomic and checkout develop"
+echo -e "${GREEN}Move into atomic and checkout develop"
 cd web/themes/contrib/atomic
 git checkout develop
-echo "npm ci"
+echo -e "${GREEN}npm ci${ENDCOLOR}"
 npm ci
-echo "Delete installed component library"
+echo -e "${GREEN}Delete installed component library${ENDCOLOR}"
 rm -rf node_modules/@yalesites-org/component-library-twig/
-echo "Clone component library"
+echo -e "${GREEN}Clone component library${ENDCOLOR}"
 [ ! -d "component-library-twig" ] && git clone git@github.com:yalesites-org/component-library-twig.git
-echo "Move into component library"
+echo -e "${GREEN}Move into component library${ENDCOLOR}"
 cd component-library-twig
-echo "Checkout the specified branch"
+echo -e "${GREEN}Checkout the specified branch${ENDCOLOR}"
 git checkout $BRANCH
 git pull
-echo "npm ci and npm run build"
+echo -e "${GREEN}npm ci and npm run build${ENDCOLOR}"
 npm ci
 npm run build
-echo "Move into theme and create empty component-library-twig directory"
+echo -e "${GREEN}Move into theme and create empty component-library-twig directory${ENDCOLOR}"
 cd ..
 mkdir node_modules/@yalesites-org/component-library-twig
-echo "Copy built dist folder"
+echo -e "${GREEN}Copy built dist folder${ENDCOLOR}"
 cp -rp component-library-twig/dist node_modules/@yalesites-org/component-library-twig/.
-echo "Copy built components folder"
+echo -e "${GREEN}Copy built components folder${ENDCOLOR}"
 cp -r component-library-twig/components node_modules/@yalesites-org/component-library-twig/.
