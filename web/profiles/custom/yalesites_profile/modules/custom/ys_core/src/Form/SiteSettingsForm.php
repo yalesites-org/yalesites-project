3ce28112d1b4f866d5d9e4f4a515a1c9831a61cd
Jim Vomero <jim.vomero@fourkitchens.com>
2022-09-09 00:21:46 -0400

    feat (YALB-681): add news and events page settingns

--- web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Form/SiteSettingsForm.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Form/SiteSettingsForm.php
@@ -13,6 +13,10 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Form for managing sitewide settings.
  *
+ * This form recreates some of the logic from the Drupal Site Information form
+ * and may need to be updated if the core form changes. See:
+ * \Drupal\system\Form\SiteInformationForm.
+ *
  * @package Drupal\ys_core\Form
  */
 class SiteSettingsForm extends ConfigFormBase {
@@ -80,18 +84,20 @@ class SiteSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return ['system.site'];
+    return ['system.site', 'ys_core.site'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('system.site');
+    $siteConfig = $this->config('system.site');
+    $yaleConfig = $this->config('ys_core.site');
+
     $form['site_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Site name'),
-      '#default_value' => $config->get('name'),
+      '#default_value' => $siteConfig->get('name'),
       '#required' => TRUE,
     ];
 
@@ -99,7 +105,7 @@ class SiteSettingsForm extends ConfigFormBase {
       '#type' => 'textfield',
       '#description' => $this->t("The From address in automated emails sent during registration and new password requests, and other notifications. (Use an address ending in your site's domain to help prevent this email being flagged as spam.)"),
       '#title' => $this->t('Site email'),
-      '#default_value' => $config->get('mail'),
+      '#default_value' => $siteConfig->get('mail'),
       '#required' => TRUE,
     ];
 
@@ -107,22 +113,38 @@ class SiteSettingsForm extends ConfigFormBase {
       '#type' => 'textfield',
       '#description' => $this->t("Specify a relative URL to display as the front page."),
       '#title' => $this->t('Front page'),
-      '#default_value' => $config->get('page')['front'],
+      '#default_value' => $siteConfig->get('page')['front'],
       '#required' => TRUE,
+    ];
+
+    $form['site_page_news'] = [
+      '#type' => 'textfield',
+      '#description' => $this->t("Specify a relative URL to display as the news landing page."),
+      '#title' => $this->t('News landing page'),
+      '#default_value' => $yaleConfig->get('page')['news'],
+      '#required' => FALSE,
+    ];
+
+    $form['site_page_events'] = [
+      '#type' => 'textfield',
+      '#description' => $this->t("Specify a relative URL to display as the events calenndar page."),
+      '#title' => $this->t('Events calendar page'),
+      '#default_value' => $yaleConfig->get('page')['events'],
+      '#required' => FALSE,
     ];
 
     $form['site_page_403'] = [
       '#type' => 'textfield',
       '#description' => $this->t('This page is displayed when the requested document is denied to the current user. Leave blank to display a generic "access denied" page.'),
       '#title' => $this->t('403 page'),
-      '#default_value' => $config->get('page')['403'],
+      '#default_value' => $siteConfig->get('page')['403'],
     ];
 
     $form['site_page_404'] = [
       '#type' => 'textfield',
       '#description' => $this->t('This page is displayed when no other content matches the requested document. Leave blank to display a generic "page not found" page.'),
       '#title' => $this->t('404 page'),
-      '#default_value' => $config->get('page')['404'],
+      '#default_value' => $siteConfig->get('page')['404'],
     ];
 
     return parent::buildForm($form, $form_state);
@@ -132,45 +154,28 @@ class SiteSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    // Validate front page path.
-    if (($value = $form_state->getValue('site_page_front')) && $value[0] !== '/') {
-      $form_state->setErrorByName('site_page_front', $this->t("The path '%path' has to start with a slash.", ['%path' => $form_state->getValue('site_page_front')]));
-    }
-    if (!$this->pathValidator->isValid($form_state->getValue('site_page_front'))) {
-      $form_state->setErrorByName('site_page_front', $this->t("Either the path '%path' is invalid or you do not have access to it.", ['%path' => $form_state->getValue('site_page_front')]));
-    }
-    // Get the normal paths of both error pages.
+    // Validate front, news, and event page paths.
+    $this->validateStartWithSlash($form_state, 'site_page_front');
+    $this->validateStartWithSlash($form_state, 'site_page_news');
+    $this->validateStartWithSlash($form_state, 'site_page_events');
+    $this->validatePath($form_state, 'site_page_front');
+
+    // Get the normal paths of error pages.
     if (!$form_state->isValueEmpty('site_page_403')) {
       $form_state->setValueForElement($form['site_page_403'], $this->aliasManager->getPathByAlias($form_state->getValue('site_page_403')));
     }
     if (!$form_state->isValueEmpty('site_page_404')) {
       $form_state->setValueForElement($form['site_page_404'], $this->aliasManager->getPathByAlias($form_state->getValue('site_page_404')));
     }
-    if (($value = $form_state->getValue('site_page_403')) && $value[0] !== '/') {
-      $form_state->setErrorByName('site_page_403', $this->t("The path '%path' has to start with a slash.", ['%path' => $form_state->getValue('site_page_403')]));
-    }
-    if (($value = $form_state->getValue('site_page_404')) && $value[0] !== '/') {
-      $form_state->setErrorByName('site_page_404', $this->t("The path '%path' has to start with a slash.", ['%path' => $form_state->getValue('site_page_404')]));
-    }
-    // Validate 403 error path.
-    if (!$form_state->isValueEmpty('site_page_403') && !$this->pathValidator->isValid($form_state->getValue('site_page_403'))) {
-      $form_state->setErrorByName('site_page_403', $this->t("Either the path '%path' is invalid or you do not have access to it.", ['%path' => $form_state->getValue('site_page_403')]));
-    }
-    // Validate 404 error path.
-    if (!$form_state->isValueEmpty('site_page_404') && !$this->pathValidator->isValid($form_state->getValue('site_page_404'))) {
-      $form_state->setErrorByName('site_page_404', $this->t("Either the path '%path' is invalid or you do not have access to it.", ['%path' => $form_state->getValue('site_page_404')]));
-    }
+
+    // Validate error page paths.
+    $this->validateStartWithSlash($form_state, 'site_page_404');
+    $this->validateStartWithSlash($form_state, 'site_page_403');
+    $this->validatePath($form_state, 'site_page_404');
+    $this->validatePath($form_state, 'site_page_403');
+
     // Email validations.
-    if (($value = $form_state->getValue('site_mail'))) {
-      // Validate email format.
-      if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-        $form_state->setErrorByName('site_mail', $this->t('Email format for "%email" is not valid. Expected format is "user@yale.edu".', ['%email' => $form_state->getValue('site_mail')]));
-      }
-      // Validate yale email.
-      if (strpos($value, '@yale.edu') === FALSE) {
-        $form_state->setErrorByName('site_mail', $this->t('Email domain has to be @yale.edu.'));
-      }
-    }
+    $this->validateEmail($form_state, 'site_mail');
 
     parent::validateForm($form, $form_state);
   }
@@ -179,17 +184,91 @@ class SiteSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Retrieve the configuration.
     $this->configFactory->getEditable('system.site')
-      // Set the submitted configuration setting.
       ->set('name', $form_state->getValue('site_name'))
       ->set('mail', $form_state->getValue('site_mail'))
       ->set('page.front', $form_state->getValue('site_page_front'))
       ->set('page.403', $form_state->getValue('site_page_403'))
       ->set('page.404', $form_state->getValue('site_page_404'))
       ->save();
+    $this->configFactory->getEditable('ys_core.site')
+      ->set('page.news', $form_state->getValue('site_page_news'))
+      ->set('page.events', $form_state->getValue('site_page_events'))
+      ->save();
 
     parent::submitForm($form, $form_state);
   }
 
+  /**
+   * Check that a submitted value starts with a slash.
+   *
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state passed by reference.
+   * @param string $fieldId
+   *   The id of a field on the connfig form.
+   *
+   * @return void
+   */
+  protected function validateStartWithSlash(&$form_state, $fieldId) {
+    if (($value = $form_state->getValue($fieldId)) && $value[0] !== '/') {
+      $form_state->setErrorByName(
+        $fieldId,
+        $this->t(
+          "The path '%path' has to start with a slash.",
+         ['%path' => $form_state->getValue($fieldId)]
+        )
+      );
+    }
+  }
+
+  /**
+   * Check that a submitted value represents a valid Drupal path.
+   *
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state passed by reference.
+   * @param string $fieldId
+   *   The id of a field on the connfig form.
+   *
+   * @return void
+   */
+  protected function validatePath(&$form_state, $fieldId) {
+    if (!$this->pathValidator->isValid($form_state->getValue($fieldId))) {
+      $form_state->setErrorByName(
+        $fieldId,
+        $this->t(
+          "Either the path '%path' is invalid or you do not have access to it.",
+          ['%path' => $form_state->getValue($fieldId)]
+        )
+      );
+    }
+  }
+
+  /**
+   * Check that a submitted value matches the format of a valid Yale email.
+   *
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state passed by reference.
+   * @param string $fieldId
+   *   The id of a field on the connfig form.
+   *
+   * @return void
+   */
+  protected function validateEmail(&$form_state, $fieldId) {
+    if (($value = $form_state->getValue($fieldId))) {
+      if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+        $form_state->setErrorByName(
+          $fieldId,
+          $this->t(
+            'Email format for "%email" is not valid. Expected format is "user@yale.edu".',
+            ['%email' => $form_state->getValue('site_mail')]
+          )
+        );
+      }
+      if (strpos($value, '@yale.edu') === FALSE) {
+        $form_state->setErrorByName(
+          $fieldId, $this->t('Email domain has to be @yale.edu.')
+        );
+      }
+    }
+  }
 }
