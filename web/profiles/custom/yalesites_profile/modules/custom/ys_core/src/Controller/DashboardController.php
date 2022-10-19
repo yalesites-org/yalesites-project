effee47ea32c352cebcd176f636ea9251c7084b5
Jim Vomero <jim.vomero@fourkitchens.com>
2022-10-18 21:41:14 -0400

    chore (YALB-756): rename structures

--- web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Controller/DashboardController.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Controller/DashboardController.php
@@ -5,27 +5,17 @@ namespace Drupal\ys_core\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
- * Give me a description.
+ * Controller for the dashboard page.
  */
 class DashboardController extends ControllerBase {
 
   /**
-   * Returns a render-able array for a test page.
+   * Dashboard page contents.
    */
   public function content() {
-
-    // Do something with your variables here.
-    $myText = 'This is not just a default text!';
-    $myNumber = 1;
-    $myArray = [1, 2, 3];
-
     return [
-      // Your theme hook name.
-      '#theme' => 'ys_dashboard_theme_hook',
-      // Your variables.
-      '#variable1' => $myText,
-      '#variable2' => $myNumber,
-      '#variable3' => $myArray,
+      '#theme' => 'ys_dashboard',
     ];
   }
+
 }
