c990a1f6ec82b42fb413869d724e804ef345e34e
Marc Berger <marc@fourkitchens.com>
2023-02-10 10:06:43 -0800

    refactor(YALB-651): Remove unused classes and variables

--- web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/EventSubscriber/NodeAccessEventSubscriber.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/EventSubscriber/NodeAccessEventSubscriber.php
@@ -7,7 +7,6 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Drupal\path_alias\AliasManager;
 
 /**
  * Overrides the 403 response to perform a CAS redirect for specific pages.
@@ -22,30 +21,13 @@ class NodeAccessEventSubscriber extends HttpExceptionSubscriberBase {
   protected $currentUser;
 
   /**
-   * The path alias manager.
-   *
-   * @var \Drupal\path_alias\AliasManager
-   */
-  protected $pathAlias;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManager
-   */
-  protected $entityTypeManager;
-
-  /**
    * The construct for this class.
    *
    * @param Drupal\Core\Session\AccountInterface $current_user
    *   The current user service.
-   * @param Drupal\path_alias\AliasManager $path_alias
-   *   The path alias manager.
    */
-  public function __construct(AccountInterface $current_user, AliasManager $path_alias) {
+  public function __construct(AccountInterface $current_user) {
     $this->currentUser = $current_user;
-    $this->pathAlias = $path_alias;
   }
 
   /**
@@ -64,18 +46,14 @@ class NodeAccessEventSubscriber extends HttpExceptionSubscriberBase {
   public function on403(ExceptionEvent $event) {
     if ($this->currentUser->isAnonymous()) {
 
-      // Get path alias.
-      $path = $event->getRequest()->getPathInfo();
-
       // Make sure this is a node.
       if ($node = $event->getRequest()->attributes->get('node')) {
-
         /* Check to see if node is set to require login only.
          * If so, redirect to CAS login.
          */
         if ($node->hasField('field_login_required')) {
           if (!$node->field_login_required->isEmpty()) {
-            $casRedirectUrl = Url::fromRoute('cas.login', ['destination' => $path])->toString();
+            $casRedirectUrl = Url::fromRoute('cas.login', ['destination' => $node->toUrl()->toString()])->toString();
             $returnResponse = new RedirectResponse($casRedirectUrl);
             $event->setResponse($returnResponse);
           }
