396dcb71e05a6a6877d73f34bd2ad2d5e3139b14
Joe Tower <joe@fourkitchens.com>
2023-04-24 10:43:44 -0500

    fix(yalb-984): update to new themes

--- web/profiles/custom/yalesites_profile/modules/custom/ys_themes/src/ThemeSettingsManager.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_themes/src/ThemeSettingsManager.php
@@ -34,12 +34,11 @@ class ThemeSettingsManager {
     'pull_quote_color' => [
       'name' => 'Pull Quote Color',
       'values' => [
-        'gray-200' => 'Light Gray',
-        'gray-500' => 'Gray',
-        'blue-yale' => 'Blue',
-        'accent' => 'Accent',
+        'one' => 'One',
+        'two' => 'Two',
+        'three' => 'Three',
       ],
-      'default' => 'gray-500',
+      'default' => 'one',
     ],
     'line_color' => [
       'name' => 'Line Color',
@@ -78,22 +77,22 @@ class ThemeSettingsManager {
     'header_theme' => [
       'name' => 'Header Theme',
       'values' => [
-        'white' => 'White',
-        'gray-100' => 'Light Gray',
-        'blue-yale' => 'Blue',
+        'one' => 'One',
+        'two' => 'Two',
+        'three' => 'Three',
       ],
-      'default' => 'white',
+      'default' => 'one',
     ],
     'footer_theme' => [
       'name' => 'Footer Theme',
       'values' => [
-        'white' => 'White',
-        'gray-100' => 'Light Gray',
-        'gray-700' => 'Gray',
-        'gray-800' => 'Dark Gray',
-        'blue-yale' => 'Blue',
+        'one' => 'One',
+        'two' => 'Two',
+        'three' => 'Three',
+        'four' => 'Four',
+        'five' => 'Five',
       ],
-      'default' => 'gray-700',
+      'default' => 'one',
     ],
   ];
 
