cc33b6ceb3693754ed8e6591c2cd01532411141f
David Blankenship <david.blankenship@yale.edu>
2023-05-18 12:50:52 -0400

    feat(YALB-1197): add soundcloud twig template

--- web/profiles/custom/yalesites_profile/modules/custom/ys_embed/src/Plugin/EmbedSource/SoundCloud.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_embed/src/Plugin/EmbedSource/SoundCloud.php
@@ -38,4 +38,11 @@ class SoundCloud extends EmbedSourceBase implements EmbedSourceInterface {
    */
   protected static $example = '<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/320687463&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>';
 
+  public function build(array $params): array {
+    return [
+      '#theme' => 'soundcloud',
+      '#track_id' => $params['track_id'],
+      '#title' => $params['title'],
+    ];
+  }
 }
