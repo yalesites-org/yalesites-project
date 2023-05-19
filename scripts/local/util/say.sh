b532973a08d271673080846d4232d17b8cc42238
David Blankenship <david.blankenship@yale.edu>
2023-05-19 09:14:47 -0400

    feat(YALB-971): script name displayed
    Now the script that is calling the message will be displayed, giving a
    sort of basic foreman type look and feel.
    
    References: https://github.com/ddollar/foreman

--- scripts/local/util/say.sh
+++ scripts/local/util/say.sh
@@ -5,9 +5,16 @@ RED='\033[0;31m'
 ENDCOLOR='\033[0m'
 
 function _say() {
-    echo -e "${GREEN}$1${ENDCOLOR}"
+    local script_name="${0##*/}"
+    echo -e "${GREEN}[$script_name] $1${ENDCOLOR}"
 }
 
 function _error() {
-    echo -e "${RED}$1${ENDCOLOR}"
+    local script_name="${0##*/}"
+    echo -e "${RED}[$script_name] $1${ENDCOLOR}"
+}
+
+function _error_and_exit() {
+    _error "$1"
+    exit
 }
