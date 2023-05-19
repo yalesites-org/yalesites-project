89eb5c632bb1c03c960cf00e2f6cb31101c77a4b
David Blankenship <david.blankenship@yale.edu>
2023-05-19 13:32:22 -0400

    feat(YALB-971): atomic changes cause drush cr
    If we are changing branches on atomic, a drush cr must be done so that
    it refreshes the theme.  This captures this and performs this cache redo
    at the end of the script.

--- scripts/local/local-git-checkout.sh
+++ scripts/local/local-git-checkout.sh
@@ -5,6 +5,20 @@ cl_branch="main"
 token_branch="main"
 atomic_branch="main"
 debug=false
+verbose=false
+
+# current_branch_for_path
+#
+# This function will return the current branch for a given path.
+#
+# params:
+#  git_path: the path to the repo
+#
+function current_branch_for_path() {
+  local git_path=${1-$(pwd)}
+
+  return $(git -C "$git_path" rev-parse --abbrev-ref HEAD)
+}
 
 # clone_or_switch_branch
 #
@@ -17,10 +31,12 @@ debug=false
 #  name: the name of the repo
 #  git_path: the path to the repo
 #  target_branch: the branch to switch to
+#
 function clone_or_switch_branch() {
   local name=$1
   local git_path=$2
   local target_branch=${3:-main}
+  local current_branch
 
   [ -z "$name" ] && _error "You must provide a name" && exit 1
   [ -z "$git_path" ] && _error "You must provide a git_path" && exit 1
@@ -31,7 +47,7 @@ function clone_or_switch_branch() {
     git clone git@github.com:yalesites-org/"$name".git "$git_path" -b "$target_branch"
 
   # Get current branch of repo
-  current_branch=$(git -C "$git_path" rev-parse --abbrev-ref HEAD)
+  current_branch=$(current_branch_for_path "$git_path")
 
   # If current branch is not the target branch
   if [ "$current_branch" != "$target_branch" ]; then
@@ -50,7 +66,7 @@ function clone_or_switch_branch() {
 }
 
 # getopts
-while getopts ":dc:t:a:" opt; do
+while getopts ":dvc:t:a:" opt; do
   case ${opt} in
     d )
       debug=true
@@ -63,6 +79,9 @@ while getopts ":dc:t:a:" opt; do
       ;;
     a )
       atomic_branch=$OPTARG
+      ;;
+    v )
+      verbose=true
       ;;
     \? )
       echo "Usage: $0 [-d] [-c <component-library-branch>] [-t <tokens-branch>] [-a <atomic-branch>]"
@@ -80,14 +99,21 @@ done
 source ./scripts/local/util/say.sh
 
 [ "$debug" = true ] && _say "Debug mode enabled" && set -x
+[ "$verbose" = true ] && _say "Verbose mode enabled"
 
 _say "Let the magic begin!"
 _say "********************"
 
+atomic_changed=false
+# If current branch did change
+if [ "$(current_branch_for_path 'web/themes/contrib/atomic')" != "$atomic_branch" ]; then
+  atomic_changed=true
+fi
+
 _say "Attempting to clone $atomic_branch branch of atomic repo"
 clone_or_switch_branch "atomic" "web/themes/contrib/atomic" "$atomic_branch"
 
-_say "Moving to atomic repo"
+[ "$verbose" = true ] && _say "Moving to atomic repo"
 cd web/themes/contrib/atomic || (_error "Could not find atomic theme. Are you in the right directory?" && exit 1)
 
 _say "Attempting to clone $token_branch branch of tokens repo"
@@ -97,23 +123,25 @@ _say "Moving into tokens repo and creating a global npm link"
 cd _yale-packages/tokens || (_error "Could not find tokens repo. Are you in the right directory?" && exit 1)
 npm link
 
-_say "Moving back to atomic"
+[ "$verbose" = true ] && _say "Moving back to atomic"
 cd ../..
 
 _say "Attempting to clone $cl_branch branch of component-library-twig repo"
 clone_or_switch_branch "component-library-twig" "_yale-packages/component-library-twig" "$cl_branch"
 
-_say "Moving into component library and creating a global npm link"
+[ "$verbose" = true ] && _say "Moving into component library"
 cd _yale-packages/component-library-twig || (_error "Could not find component-library-twig repo. Are you in the right directory?" && exit 1)
+
+_say "Using the tokens global npm link inside the component library"
 npm link
 
-_say "Moving back to atomic"
+[ "$verbose" = true ] && _say "Moving back to atomic"
 cd ../..
 
 _say "Using the component-library global npm link inside the atomic theme"
 npm link @yalesites-org/component-library-twig
 
-_say "Moving into the component library"
+[ "$verbose" = true ] && _say "Moving into the component library"
 cd _yale-packages/component-library-twig || (_error "Could not find component-library-twig repo. Are you in the right directory?" && exit 1)
 
 _say "Running clean install so we can patch Twig.js"
@@ -122,12 +150,13 @@ npm ci -y
 _say "Using the tokens global npm link inside the component library"
 npm link @yalesites-org/tokens
 
+[ "$verbose" = true ] && _say "Moving back to atomic"
 cd ../..
 
 _say "Attempting to npm link tokens inside atomic"
 npm link @yalesites-org/tokens
 
-_say "Moving to tokens repo"
+[ "$verbose" = true ] && _say "Moving to tokens repo"
 cd _yale-packages/tokens || (_error "Could not find tokens repo. Are you in the right directory?" && exit 1)
 
 _say "Building tokens"
@@ -139,3 +168,6 @@ cd ../../../../../..
 [ ! -L "atomic" ] && ln -s web/themes/contrib/atomic atomic
 [ ! -L "component-library-twig" ] && ln -s atomic/_yale-packages/component-library-twig component-library-twig
 [ ! -L "tokens" ] && ln -s atomic/_yale-packages/tokens tokens
+
+[ "$atomic_changed" = true ] && _say "Atomic theme changed, so we need to clear Drupal cache; this could take a while" && lando drush cr
+
