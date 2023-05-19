9e8c64c2d3dcaea9887e8548ae2e984a77e8dcf9
David Blankenship <david.blankenship@yale.edu>
2023-05-19 08:36:31 -0400

    feat(YALB-971): Add error message util
    If we have error messages, let's display them in a different way as
    well.

--- scripts/local/util/say.sh
+++ scripts/local/util/say.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 ENDCOLOR='\033[0m'
 
 function _say() {
     echo -e "${GREEN}$1${ENDCOLOR}"
+}
+
+function _error() {
+    echo -e "${RED}$1${ENDCOLOR}"
 }
