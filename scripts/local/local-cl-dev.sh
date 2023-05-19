144e574d5ddd444c3cc1e049523bb38265e41aee
David Blankenship <david.blankenship@yale.edu>
2023-05-19 11:58:14 -0400

    feat(YALB-971): add git checkout script
    This creates a script out of the need to be able to switch branches of
    any of the repos at any time.  The new script, local-git-checkout.sh,
    allows you to switch any sub-repo you wish to the version you want,
    while exiting should there be uncommitted changes so you can take care
    of it first and not lose any work.
    
    Note, at this point, I have not tested how well it actually functions on
    a drupal site side, only from ensuring it accuately switches branches.

--- scripts/local/local-cl-dev.sh
+++ scripts/local/local-cl-dev.sh
@@ -11,46 +11,14 @@
 # through the regular component library release process, and be included in an
 # official release of Atomic.
 
-[ $# -eq 1 ] && [ "$1" == "-h" ] && echo "Usage: $0 [-d]" && exit
-[ $# -eq 1 ] && [ "$1" == "-d" ] && _say "Debug mode enabled" && set -x
-
-[ -f "./scripts/local/util/say.sh" ] || (echo -e "You must run this from the yalesites root directory" && exit)
+[ -e "./scripts/local/util/say.sh" ] || (echo -e "[$0] Say utility not found.  You must run this from the yalesites root directory: " && exit 1)
 source ./scripts/local/util/say.sh
 
- _say "Moving to atomic repo"
-cd web/themes/contrib/atomic || (_error "Could not find atomic theme. Are you in the right directory?" && exit)
+if ! ./scripts/local/local-git-checkout.sh -c develop -a develop; then
+  exit 1
+fi
 
-_say "Attempting to clone tokens repo"
-[ ! -d "_yale-packages/tokens" ] && git clone git@github.com:yalesites-org/tokens.git _yale-packages/tokens
-
-_say "Moving into tokens repo and creating a global npm link"
-cd _yale-packages/tokens || (_error "Could not find tokens repo. Are you in the right directory?" && exit)
-npm link
-
-_say "Moving back to atomic"
-cd ../..
-
-_say "Attempting to clone component-library-twig repo"
-[ ! -d "_yale-packages/component-library-twig" ] && git clone git@github.com:yalesites-org/component-library-twig.git _yale-packages/component-library-twig
-
-_say "Moving into component library and creating a global npm link"
-cd _yale-packages/component-library-twig || (_error "Could not find component-library-twig repo. Are you in the right directory?" && exit)
-npm link
-
-_say "Moving back to atomic"
-cd ../..
-
-_say "Using the component-library global npm link inside the atomic theme"
-npm link @yalesites-org/component-library-twig
-
-_say "Moving into the component library"
-cd _yale-packages/component-library-twig || (_error "Could not find component-library-twig repo. Are you in the right directory?" && exit)
-
-_say "Running clean install so we can patch Twig.js"
-npm ci -y
-
-_say "Using the tokens global npm link inside the component library"
-npm link @yalesites-org/tokens
+cd atomic || exit
 
 _say "Running the develop script in the component library"
 npm run develop
