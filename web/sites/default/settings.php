e0b13e42854f879f0e17b6f5106f7702041e31d3
nJim <jamesvomero@gmail.com>
2023-05-23 10:58:34 -0400

    chore: update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -26,6 +26,17 @@ include __DIR__ . "/settings.pantheon.php";
 // $settings['skip_permissions_hardening'] = TRUE;
 
 /**
+ * Override config with values from the secrets file.
+ */
+$secrets_json_text = file_get_contents('/files/private/secrets.json');
+$secrets_data = json_decode($secrets_json_text, TRUE);
+$config['recaptcha.settings']['site_key'] = $secrets_data['recaptcha_v2_key'];
+$config['recaptcha.settings']['secret_key'] = $secrets_data['recaptcha_v2_secret'];
+$config['recaptcha_v3.settings']['site_key'] = $secrets_data['recaptcha_v3_key'];
+$config['recaptcha_v3.settings']['secret_key'] = $secrets_data['recaptcha_v3_secret'];
+$config['mailchimp_transactional.settings']['mailchimp_transactional_api_key'] = $secrets_data['mailchimp_transactional_api_key'];
+
+/**
  * If there is a local settings file, then include it
  */
 $local_settings = __DIR__ . "/settings.local.php";
@@ -35,3 +46,4 @@ if (file_exists($local_settings)) {
 
 // Set the install profile as the source of site config.
 $settings['config_sync_directory'] = 'profiles/custom/yalesites_profile/config/sync';
+
