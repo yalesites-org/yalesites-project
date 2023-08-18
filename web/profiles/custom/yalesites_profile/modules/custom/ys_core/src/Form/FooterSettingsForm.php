60f9539be044797022c5c85b4220a50a752bd619
Marc Berger <marc@fourkitchens.com>
2023-08-17 18:10:30 -0700

    feat(YALB-1485): WIP Saving config values and form validation for links

--- web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Form/FooterSettingsForm.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Form/FooterSettingsForm.php
@@ -6,6 +6,7 @@ use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\ys_core\SocialLinksManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -51,7 +52,8 @@ class FooterSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
 
     $form = parent::buildForm($form, $form_state);
-    $config = $this->config('ys_core.social_links');
+    $socialConfig = $this->config('ys_core.social_links');
+    $footerConfig = $this->config('ys_core.footer_settings');
 
     $form['#attached']['library'][] = 'ys_core/footer_settings_form';
 
@@ -85,22 +87,30 @@ class FooterSettingsForm extends ConfigFormBase {
 
     $form['footer_content']['footer_logos'] = [
       '#type' => 'media_library',
-      '#allowed_bundles' => ['image'],
       '#title' => $this->t('Footer logos'),
+      '#allowed_bundles' => ['image'],
       '#required' => FALSE,
       '#cardinality' => 4,
-      //'#default_value' => ($yaleConfig->get('image_fallback')) ? $yaleConfig->get('image_fallback')['teaser'] : NULL,
-      //'#description' => $this->t('This image will be used for event and post card displays when no teaser image is selected.'),
+      '#default_value' => ($footerConfig->get('content.logos')) ? implode(',', $footerConfig->get('content.logos')) : NULL,
+    ];
+
+    $form['footer_content']['footer_text'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Text Content'),
+      '#format' => 'restricted_html',
+      '#default_value' => (isset($footerConfig->get('content.text')['value'])) ? $footerConfig->get('content.text')['value'] : NULL,
     ];
 
     $form['footer_links']['links_col_1_heading'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Link Column 1 Heading'),
+      '#default_value' => ($footerConfig->get('links.links_col_1_heading')) ? $footerConfig->get('links.links_col_1_heading') : NULL,
     ];
 
     $form['footer_links']['links_col_2_heading'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Link Column 2 Heading'),
+      '#default_value' => ($footerConfig->get('links.links_col_2_heading')) ? $footerConfig->get('links.links_col_2_heading') : NULL,
     ];
 
     $form['footer_links']['links_col_1'] = [
@@ -112,7 +122,7 @@ class FooterSettingsForm extends ConfigFormBase {
         '#title' => $this->t('URL'),
         '#description' => $this->t('Type the URL or autocomplete for internal paths.'),
         '#autocomplete_route_name' => 'linkit.autocomplete',
-        '#default_value' => $config->get('alert.link_url'),
+        //'#default_value' => $config->get('alert.link_url'),
         '#autocomplete_route_parameters' => [
           'linkit_profile_id' => 'default',
         ],
@@ -132,7 +142,7 @@ class FooterSettingsForm extends ConfigFormBase {
         '#title' => $this->t('URL'),
         '#description' => $this->t('Type the URL or autocomplete for internal paths.'),
         '#autocomplete_route_name' => 'linkit.autocomplete',
-        '#default_value' => $config->get('alert.link_url'),
+        //'#default_value' => $config->get('alert.link_url'),
         '#autocomplete_route_parameters' => [
           'linkit_profile_id' => 'default',
         ],
@@ -147,11 +157,19 @@ class FooterSettingsForm extends ConfigFormBase {
       $form['social_links'][$id] = [
         '#type' => 'url',
         '#title' => $this->t('@name URL', ['@name' => $name]),
-        '#default_value' => $config->get($id),
+        '#default_value' => $socialConfig->get($id),
       ];
     }
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $this->validateFooterLinks($form_state, 'links_col_1');
+    $this->validateFooterLinks($form_state, 'links_col_2');
   }
 
   /**
@@ -163,11 +181,22 @@ class FooterSettingsForm extends ConfigFormBase {
    *   Form state.
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $config = $this->config('ys_core.social_links');
+    // Social config.
+    $socialConfig = $this->config('ys_core.social_links');
     foreach ($this->socialLinks::SITES as $id => $name) {
-      $config->set($id, $form_state->getValue($id));
+      $socialConfig->set($id, $form_state->getValue($id));
     }
-    $config->save();
+    $socialConfig->save();
+
+    // Footer settings config.
+    $footerConfig = $this->config('ys_core.footer_settings');
+    $footerConfig->set('content.logos', explode(',', $form_state->getValue('footer_logos')));
+    $footerConfig->set('content.text', $form_state->getValue('footer_text'));
+    $footerConfig->set('links.links_col_1_heading', $form_state->getValue('links_col_1_heading'));
+    $footerConfig->set('links.links_col_2_heading', $form_state->getValue('links_col_2_heading'));
+
+    $footerConfig->save();
+
     $this->cacheRender->invalidateAll();
     return parent::submitForm($form, $form_state);
   }
@@ -178,6 +207,7 @@ class FooterSettingsForm extends ConfigFormBase {
   protected function getEditableConfigNames() {
     return [
       'ys_core.social_links',
+      'ys_core.footer_settings',
     ];
   }
 
@@ -206,6 +236,35 @@ class FooterSettingsForm extends ConfigFormBase {
     parent::__construct($config_factory);
     $this->cacheRender = $cache_render;
     $this->socialLinks = $social_links_manager;
+  }
+
+  protected function validateFooterLinks($form_state, $field_id) {
+    if (($value = $form_state->getValue($field_id))) {
+      foreach ($value as $index => $link) {
+
+      //   if (empty($link['link_url'])) {
+      //     $form_state->setErrorByName(
+      //       $field_id,
+      //       $this->t(
+      //         "Links must contain a URL",
+      //         ['%path' => $form_state->getValue($field_id)]
+      //       )
+      //     );
+      //   }
+        global $base_url;
+
+        if (empty($link['link_title'])) {
+          $response = new TrustedRedirectResponse($base_url . '/admin/yalesites/footer#edit-footer-links');
+
+          $form_state->setErrorByName(
+            $field_id,
+            $this->t("Links must contain a title"),
+          );
+          $form_state->setResponse($response);
+        }
+
+      }
+    }
   }
 
 }
