f4b44ae1fc1988162b723132074b67610b09fa7d
Marc Berger <marc@fourkitchens.com>
2023-09-18 13:23:19 -0700

    fix: Footer settings form tweaks, add affiliation taxonomy description, views basic fix for older storage

--- web/profiles/custom/yalesites_profile/modules/custom/ys_views_basic/src/ViewsBasicManager.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_views_basic/src/ViewsBasicManager.php
@@ -199,6 +199,14 @@ class ViewsBasicManager extends ControllerBase implements ContainerInjectionInte
     // Set operator: "+" is "OR" and "," is "AND".
     $operator = $paramsDecoded['operator'] ?? '+';
 
+    // Fix for older setting terms for nodes not saved with the new storage.
+    if (is_array($termsIncludeArray[0])) {
+      foreach ($termsIncludeArray as $terms) {
+        $termsIncludeArrayFixed[] = $terms['target_id'];
+      }
+      $termsIncludeArray = $termsIncludeArrayFixed;
+    }
+    // End fix.
     $termsInclude = (count($termsIncludeArray) != 0) ? implode($operator, $termsIncludeArray) : 'all';
     $termsExclude = (count($termsExcludeArray) != 0) ? implode($operator, $termsExcludeArray) : NULL;
 
