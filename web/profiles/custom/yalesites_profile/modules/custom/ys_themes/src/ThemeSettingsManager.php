3a5552374c306c35d91ad24a767d75c0f37577d7
Marc Berger <marc@fourkitchens.com>
2023-09-28 17:24:56 -0700

    feat(YALB-1562): Add new header accent color picker in theme settings

--- web/profiles/custom/yalesites_profile/modules/custom/ys_themes/src/ThemeSettingsManager.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_themes/src/ThemeSettingsManager.php
@@ -120,6 +120,46 @@ class ThemeSettingsManager {
         ],
       ],
     ],
+    'header_accent' => [
+      'name' => 'Header Accent',
+      'prop_type' => 'element',
+      'selector' => 'header[data-header-accent]',
+      'default' => 'one',
+      'values' => [
+        'one' => [
+          'label' => 'One',
+          'color_theme' => 'one',
+        ],
+        'two' => [
+          'label' => 'Two',
+          'color_theme' => 'two',
+        ],
+        'three' => [
+          'label' => 'Three',
+          'color_theme' => 'three',
+        ],
+        'four' => [
+          'label' => 'Four',
+          'color_theme' => 'four',
+        ],
+        'five' => [
+          'label' => 'Five',
+          'color_theme' => 'five',
+        ],
+        'six' => [
+          'label' => 'Six',
+          'color_theme' => 'six',
+        ],
+        'seven' => [
+          'label' => 'Seven',
+          'color_theme' => 'seven',
+        ],
+        'eight' => [
+          'label' => 'Eight',
+          'color_theme' => 'eight',
+        ],
+      ],
+    ],
     'footer_theme' => [
       'name' => 'Footer Theme',
       'prop_type' => 'element',
