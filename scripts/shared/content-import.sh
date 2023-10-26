41ca5920ba6674d2e54de224a67e49ad11766d70
Vincent Massaro <vincent.massaro@yale.edu>
2023-10-26 14:29:12 -0400

    fix(YALB-1588): add env var to detect if running under lando

--- scripts/shared/content-import.sh
+++ scripts/shared/content-import.sh
@@ -9,18 +9,14 @@ curl -s -L -o "$STARTERKIT_FILE" https://github.com/yalesites-org/yalesites-star
 # Check if running under lando, otherwise assume CI.
 # Put starterkit file in place and import.
 # Clean up, and set front page in config.
-if [ "$LANDO" == "ON" ]; then
+if [ "$YALESITES_INSTALL" == "1" ]; then
   lando drush content:import ../"$STARTERKIT_FILE"
   lando drush cset system.site page.front '/homepage'
   rm "$STARTERKIT_FILE"
 else
   COMMAND=$(terminus connection:info "$SITE_MACHINE_NAME".dev --field=sftp_command)
-
   eval "$COMMAND:/files/ <<< 'put $STARTERKIT_FILE'"
-
   terminus drush "$SITE_MACHINE_NAME".dev -- content:import ../../files/"$STARTERKIT_FILE"
-
   eval "$COMMAND:/files/ <<< 'rm $STARTERKIT_FILE'"
-
   terminus drush "$SITE_MACHINE_NAME".dev -- cset system.site page.front '/homepage' -y
 fi
