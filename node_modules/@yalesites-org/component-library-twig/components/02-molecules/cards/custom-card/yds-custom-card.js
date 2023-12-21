e8dd36e0910032a90b0e714b90a630e2fc6fd54e
nJim <nJim@users.noreply.github.com>
2023-12-21 15:26:07 +0000

    build: commit compiled component library

---
+++ node_modules/@yalesites-org/component-library-twig/components/02-molecules/cards/custom-card/yds-custom-card.js
@@ -0,0 +1,36 @@
+Drupal.behaviors.customCard = {
+  attach(context) {
+    // Inspiration and reasoning for this JavaScript can be found in the Cards
+    // chapter of the book linked below:
+    // https://inclusive-components.design/cards/#theredundantclickevent
+    // Selectors
+    const customCards = context.querySelectorAll('.custom-card');
+
+    customCards.forEach((customCard) => {
+      const card = customCard;
+      const link = card.querySelector('.custom-card__heading-link');
+
+      // If the card has a link, make the whole card clickable. However, allow
+      // users to select text by only triggering the link if the "click up" is
+      // less than 200ms from the "click down".
+      if (link) {
+        let down;
+        let up;
+
+        card.style.cursor = 'pointer';
+        card.onmousedown = () => {
+          // Calculate when the "click" starts.
+          down = +new Date();
+        };
+        card.onmouseup = () => {
+          // Calculate when the "click" ends.
+          up = +new Date();
+          // If the click "duration" is less than 200ms, trigger a click.
+          if (up - down < 200) {
+            link.click();
+          }
+        };
+      }
+    });
+  },
+};
