39744d7d2460f78e285d1a4ac91b2553408fa919
nJim <jamesvomero@gmail.com>
2024-01-10 11:05:11 -0500

    fix(YALB-1513): linter fix

--- web/profiles/custom/yalesites_profile/modules/custom/ys_layouts/src/Plugin/Block/PageMetaBlock.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_layouts/src/Plugin/Block/PageMetaBlock.php
@@ -118,7 +118,7 @@ class PageMetaBlock extends BlockBase implements ContainerFactoryPluginInterface
         'hidden' => $this->t('Hide Title: Should only be used when a page’s banner Block title is set to H1'),
       ],
       '#description' => $this->t('For more information about conditional banner titles, <a href="https://yalesites.yale.edu/posts/2023-10-17-community-spotlight-yale-united-way-campaign#tips" target="_blank">view our tips and tricks on this topic</a>.'),
-      ];
+    ];
 
     return $form;
   }
