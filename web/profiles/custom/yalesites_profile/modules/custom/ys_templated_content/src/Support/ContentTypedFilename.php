8b98976d8c12b6547a07d9373fb963b55343ff49
David Blankenship <david.blankenship@yale.edu>
2024-01-19 17:52:48 -0500

    chore(YALB-1687): fix linting issues

--- web/profiles/custom/yalesites_profile/modules/custom/ys_templated_content/src/Support/ContentTypedFilename.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_templated_content/src/Support/ContentTypedFilename.php
@@ -6,13 +6,40 @@ namespace Drupal\ys_templated_content\Support;
  * Helper class for template filenames.
  */
 class ContentTypedFilename {
+
+  /**
+   * The original filename.
+   *
+   * @var string
+   */
   public readonly String $originalFilename;
+
+  /**
+   * The content type.
+   *
+   * @var string
+   */
   public readonly String $contentType;
+
+  /**
+   * The template name.
+   *
+   * @var string
+   */
   public readonly String $template;
+
+  /**
+   * The humanized template name.
+   *
+   * @var string
+   */
   public readonly String $humanizedTemplateName;
 
   /**
+   * ContentTypedFilename constructor.
    *
+   * @param string $filename
+   *   The filename of the form <content_type>__<template_name>.yml.
    */
   public function __construct(String $filename) {
     $this->originalFilename = $filename;
@@ -24,7 +51,10 @@ class ContentTypedFilename {
   }
 
   /**
+   * Get the name of the file without the extension.
    *
+   * @param string $filename
+   *   The filename.
    */
   protected function getNameWithoutExtension(String $filename) : String {
     $suffix = str_replace('.yml', '', $filename);
@@ -32,7 +62,10 @@ class ContentTypedFilename {
   }
 
   /**
+   * Get the content type from the filename.
    *
+   * @param string $filename
+   *   The filename.
    */
   protected function getContentType(String $filename) : String {
     $parts = explode('__', $filename);
@@ -40,7 +73,10 @@ class ContentTypedFilename {
   }
 
   /**
+   * Get the template from the filename.
    *
+   * @param string $filename
+   *   The filename.
    */
   protected function getTemplate(String $filename) : String {
     $parts = explode('__', $filename);
@@ -48,7 +84,10 @@ class ContentTypedFilename {
   }
 
   /**
+   * Get the humanized template name.
    *
+   * @param string $template
+   *   The template name of the form <under_scored_name>.
    */
   protected function humanizedName(String $template) : String {
     return ucwords(str_replace('_', ' ', $template));
