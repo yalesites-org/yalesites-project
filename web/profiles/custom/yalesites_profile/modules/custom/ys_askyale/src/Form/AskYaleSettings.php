10f42ed421aa787e38d63a69ef22b04e2100be65
Marc Berger <marc@fourkitchens.com>
2024-01-24 16:22:02 -0800

    fix(GitHub-20): Fix linting error

--- web/profiles/custom/yalesites_profile/modules/custom/ys_askyale/src/Form/AskYaleSettings.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_askyale/src/Form/AskYaleSettings.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\ys_askyale\Form;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
