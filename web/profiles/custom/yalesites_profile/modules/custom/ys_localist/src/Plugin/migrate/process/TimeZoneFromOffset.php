3874ad2a879472cb7324dc1ac97d24ec8340e94e
Marc Berger <marc@fourkitchens.com>
2024-03-08 17:03:55 -0800

    feat(YSP-388): Add the ability for dynamic URL for Localist API endpoint

--- web/profiles/custom/yalesites_profile/modules/custom/ys_localist/src/Plugin/migrate/process/TimeZoneFromOffset.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_localist/src/Plugin/migrate/process/TimeZoneFromOffset.php
@@ -19,14 +19,15 @@ class TimeZoneFromOffset extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $offset = $value;
-
-    list($hours, $minutes) = explode(':', $offset);
+    // @see https://stackoverflow.com/questions/11820718/convert-utc-offset-to-timezone-or-date
+    [$hours, $minutes] = explode(':', $value);
     $seconds = $hours * 60 * 60 + $minutes * 60;
-    // Get timezone name from seconds
+    // Get timezone name from seconds.
     $tz = timezone_name_from_abbr('', $seconds, 1);
-    // Workaround for bug #44780
-    if($tz === false) $tz = timezone_name_from_abbr('', $seconds, 0);
+    // Workaround for bug #44780.
+    if ($tz === FALSE) {
+      $tz = timezone_name_from_abbr('', $seconds, 0);
+    }
 
     return $tz;
   }
