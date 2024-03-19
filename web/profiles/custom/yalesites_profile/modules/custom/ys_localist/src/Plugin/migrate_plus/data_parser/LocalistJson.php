39f26f403a572994b18e33afd335b181b53b2d4e
Marc Berger <marc@fourkitchens.com>
2024-03-19 11:55:57 -0700

    fix(YSP-388): Fix linting error

--- web/profiles/custom/yalesites_profile/modules/custom/ys_localist/src/Plugin/migrate_plus/data_parser/LocalistJson.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_localist/src/Plugin/migrate_plus/data_parser/LocalistJson.php
@@ -47,7 +47,7 @@ class LocalistJson extends Json implements ContainerFactoryPluginInterface, Data
       $source_data = json_decode($response, TRUE, 512, JSON_THROW_ON_ERROR);
 
       // If json_decode() has returned NULL, it might be that the data isn't
-      // valid utf8 - see http://php.net/manual/en/function.json-decode.php#86997.
+      // valid utf8 see http://php.net/manual/en/function.json-decode.php#86997.
       if (is_null($source_data)) {
         $utf8response = utf8_encode($response);
         $source_data = json_decode($utf8response, TRUE, 512, JSON_THROW_ON_ERROR);
