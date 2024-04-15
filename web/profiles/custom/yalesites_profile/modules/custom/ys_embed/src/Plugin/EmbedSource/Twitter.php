37b11d77ab3d9dec3436dcda6d77ee07450a5fec
David Blankenship <david.blankenship@yale.edu>
2024-04-15 11:59:17 -0400

    fix(YSP-419): rename twitter again

--- web/profiles/custom/yalesites_profile/modules/custom/ys_embed/src/Plugin/EmbedSource/Twitter.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_embed/src/Plugin/EmbedSource/Twitter.php
@@ -10,8 +10,8 @@ use Drupal\ys_embed\Plugin\EmbedSourceInterface;
  *
  * @EmbedSource(
  *   id = "twitter",
- *   label = @Translation("X (formally Twitter) post"),
- *   description = @Translation("X (formally Twitter) embed source."),
+ *   label = @Translation("X post (formally Twitter tweet)"),
+ *   description = @Translation("X post (formally Twitter tweet) embed source."),
  *   thumbnail = "x-twitter.png",
  *   active = TRUE,
  * )
@@ -31,7 +31,7 @@ class Twitter extends EmbedSourceBase implements EmbedSourceInterface {
   /**
    * {@inheritdoc}
    */
-  protected static $instructions = 'On the X website, click the triangular (...) icon on the upper-right corner of a tweet and select the \'Embed Post\' item from the contextual menu. The embed code will appear in an input-box on this interface.';
+  protected static $instructions = 'On the X website, click the (...) icon on the upper-right corner of a post and select the \'Embed Post\' item from the contextual menu. Select \'Embedded Post\'.  The embed code will appear in an input-box on this interface.';
 
   /**
    * {@inheritdoc}
