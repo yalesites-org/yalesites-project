76d9bfa085f4e9ef1500bfa4abf47973a3074bcb
David Blankenship <david.blankenship@yale.edu>
2024-05-28 11:51:55 -0400

    feat(YSP-487): add JW Player embed

---
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_embed/src/Plugin/EmbedSource/JWPlayer.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\ys_embed\Plugin\EmbedSource;
+
+use Drupal\ys_embed\Plugin\EmbedSourceBase;
+use Drupal\ys_embed\Plugin\EmbedSourceInterface;
+
+/**
+ * JWPlayer post embed source.
+ *
+ * @EmbedSource(
+ *   id = "jwplayer",
+ *   label = @Translation("JW Player video"),
+ *   description = @Translation("JW Player embed for videos hosted on the jwplatform"),
+ *   thumbnail = "jwplayer.png",
+ *   active = TRUE,
+ * )
+ */
+class JWPlayer extends EmbedSourceBase implements EmbedSourceInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $pattern = '/<iframe.+src="(?<url>https:\/\/content.jwplatform.com\/players\/[^"]+)"/';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $template = '<iframe src="{{$url}}"></iframe>\r\n';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $instructions = 'Retrieve the jwplayer iframe embed code from jwplatform.';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $example = <<<EXAMPLE
+<div>
+<div>
+<iframe allowfullscreen="" frameborder="0" height="551" scrolling="auto" 
+src="https://content.jwplatform.com/players/2sVMfwDJ-XjbdEvEx.html" width="980">
+</iframe>
+</div>
+</div>
+<p>&nbsp;</p>
+<div>
+EXAMPLE;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $displayAttributes = [
+    'width' => '100%',
+    'height' => '100%',
+    'scrolling' => 'no',
+    'frameborder' => 'no',
+    'isIframe' => TRUE,
+  ];
+
+}
