9177245a092000d5d9de05a41f4619b80d37aabc
laura-johnson <laura@fourkitchens.com>
2024-06-10 20:00:49 -0700

    feat(YSP-431(: Add/Update Call-to-Action button utility nav (#665)
    * feat: ysp-431 add fields to header settings form
    
    * feat: ysp-431 add variables to display cta
    
    * fix: ysp-431 update label for url
    
    * fix(YSP-431): exclude utilty nav bar button from JS theme updates for button-theme selection
    
    ---------
    
    Co-authored-by: Joe Tower <joetower@gmail.com>

--- web/profiles/custom/yalesites_profile/modules/custom/ys_themes/js/levers.js
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_themes/js/levers.js
@@ -1,7 +1,8 @@
 ((Drupal) => {
   Drupal.behaviors.ysLevers = {
-    attach: function (context, settings) { // eslint-disable-line
-      once("ysLevers", ".layout-container", context).forEach((element) => {
+    attach: function (context, settings) {
+      // eslint-disable-line
+      once('ysLevers', '.layout-container', context).forEach((element) => {
         // Update root CSS variables
         function updateRoot(selector, value) {
           const prefix = selector.match(/--([a-z]*[0-9]*)/g);
@@ -17,25 +18,28 @@
           // These convert from element[data-attribute-name] to attributeName
           const dataAttribute = selector.match(/\[(.*?)\]/);
           const convertedAttributeName = dataAttribute[1]
-            .replace("data-", "")
+            .replace('data-', '')
             .replace(/-([a-z]?)/g, (m, g) => g.toUpperCase());
           for (let i = 0; i < elements.length; i++) {
-            elements[i].dataset[convertedAttributeName] = value;
+            // Skip the CTA button in the header
+            if (!elements[i].classList.contains('utility-nav__cta')) {
+              elements[i].dataset[convertedAttributeName] = value;
+            }
           }
         }
 
         function initStyleDrawer() {
           const formItems = context.querySelectorAll(
-            "input.ys-themes--setting"
+            'input.ys-themes--setting'
           );
 
           for (let i = 0; i < formItems.length; i++) {
-            formItems[i].addEventListener("click", (event) => {
+            formItems[i].addEventListener('click', (event) => {
               const { propType, selector } = event.target.dataset;
-              if (propType === "root") {
+              if (propType === 'root') {
                 updateRoot(selector, formItems[i].value);
               }
-              if (propType === "element") {
+              if (propType === 'element') {
                 updateElement(selector, formItems[i].value);
               }
             });
@@ -49,21 +53,23 @@
       });
 
       //
-      // Add Checked (checked = "checked") attribute to clicked radio elements 
+      // Add Checked (checked = "checked") attribute to clicked radio elements
       // to visually identify the active clicked theme option.
       //
 
       // Get all radio inputs in the glogal_theme name group
-      const radioInputs = document.querySelectorAll('input[name="global_theme"]');
+      const radioInputs = document.querySelectorAll(
+        'input[name="global_theme"]'
+      );
 
       // Add event listener to each radio input
-      radioInputs.forEach(input => {
-        input.addEventListener('change', function() {
+      radioInputs.forEach((input) => {
+        input.addEventListener('change', function () {
           if (this.checked) {
             this.setAttribute('checked', 'checked');
 
             // Remove the 'checked' attribute from other radio inputs
-            radioInputs.forEach(otherInput => {
+            radioInputs.forEach((otherInput) => {
               if (otherInput !== this) {
                 otherInput.removeAttribute('checked');
               }
@@ -71,12 +77,16 @@
           }
 
           // Get each component-color span element
-          const targetElements = document.querySelectorAll('span.component-color');
+          const targetElements = document.querySelectorAll(
+            'span.component-color'
+          );
 
           // Read active global theme and capture the number in a variable and
           // update it on radio input paletter selection change.
-          targetElements.forEach(targetElement => {
-            const globalTheme = document.querySelector('div[data-global-theme]');
+          targetElements.forEach((targetElement) => {
+            const globalTheme = document.querySelector(
+              'div[data-global-theme]'
+            );
             const currentTheme = globalTheme.getAttribute('data-global-theme');
             const originalBackground = targetElement.style.background;
 
@@ -84,7 +94,10 @@
             const regexPattern = new RegExp(`--global-themes-(\\w+)-`);
 
             // Replace the --global-themes-${globalTheme}- portion
-            const updatedBackground = originalBackground.replace(regexPattern, `--global-themes-${currentTheme}-`);
+            const updatedBackground = originalBackground.replace(
+              regexPattern,
+              `--global-themes-${currentTheme}-`
+            );
 
             // Update the inline style with the modified background value
             targetElement.style.background = updatedBackground;
@@ -94,18 +107,18 @@
 
       // Function to handle radio input checked behavior based on radio element selection.
       // Note: 'global_theme' is excluded because we're doing the same thing above,
-      // but adding on to it. 
+      // but adding on to it.
       function handleRadioInputs(radioGroup) {
         // Get references to the radio input elements within the specified group
         const radioInputs = document.querySelectorAll(radioGroup);
-      
+
         // Add event listener to each radio input
-        radioInputs.forEach(input => {
-          input.addEventListener('change', function() {
+        radioInputs.forEach((input) => {
+          input.addEventListener('change', function () {
             if (this.checked) {
               this.setAttribute('checked', 'checked');
               // Remove the 'checked' attribute from other radio inputs
-              radioInputs.forEach(otherInput => {
+              radioInputs.forEach((otherInput) => {
                 if (otherInput !== this) {
                   otherInput.removeAttribute('checked');
                 }
@@ -114,9 +127,9 @@
           });
         });
       }
-      
+
       // Store radio input groups in an array
-      const radioGroups = [ 
+      const radioGroups = [
         'input[name="nav_position"]',
         'input[name="nav_type"]',
         'input[name="button_theme"]',
@@ -125,9 +138,9 @@
         'input[name="footer_theme"]',
         'input[name="footer_accent"]',
       ];
-      
+
       // Apply the function to each radio input group
-      radioGroups.forEach(group => {
+      radioGroups.forEach((group) => {
         handleRadioInputs(group);
       });
     },
