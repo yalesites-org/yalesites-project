37a557a58ef712b6a0f6757b3f14245a5a1bab4f
David Blankenship <david.blankenship@yale.edu>
2024-11-07 14:03:58 -0500

    feat(YSP-737): added enable/disable checkbox

--- web/profiles/custom/yalesites_profile/modules/custom/ys_beacon/src/Form/AiSettingsForm.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_beacon/src/Form/AiSettingsForm.php
@@ -4,6 +4,7 @@ namespace Drupal\ys_beacon\Form;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\ai_engine_chat\Form\AiEngineChatSettings;
+use Drupal\ai_engine_chat\Form\AiEngineChatAdmin;
 
 /**
  * Form for AI Settings.
@@ -29,6 +30,14 @@ class AiSettingsForm extends AiEngineChatSettings {
   public function buildForm(array $form, FormStateInterface $form_state): array {
     $config = $this->config(self::CONFIG_NAME);
 
+    $adminConfig = $this->config(AiEngineChatAdmin::CONFIG_NAME);
+
+    $form['enable'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Enable chat widget'),
+      '#default_value' => $adminConfig->get('enable') ?? FALSE,
+      '#description' => $this->t('Enable or disable chat service across the site. Chat can be launched by using the href="#launch-chat" on any link.'),
+    ];
     $form['system_instructions'] = [
       '#type' => 'textarea',
       '#title' => $this->t('System Instructions'),
@@ -46,6 +55,11 @@ class AiSettingsForm extends AiEngineChatSettings {
   public function submitForm(array &$form, FormStateInterface $form_state): void {
     $this->config(self::CONFIG_NAME)
       ->set('system_instructions', $form_state->getValue('system_instructions'))
+      ->save();
+
+    $adminConfig = $this->config(AiEngineChatAdmin::CONFIG_NAME);
+    $adminConfig
+      ->set('enable', $form_state->getValue('enable'))
       ->save();
     parent::submitForm($form, $form_state);
   }
