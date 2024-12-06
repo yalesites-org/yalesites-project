8bbe66792605a06281cd80d413938a8678524e39
Marc Berger <marc@fourkitchens.com>
2024-12-06 12:50:19 -0800

    feat(IYY-283): WIP: Start work on 50/50 layouts

---
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_layouts/src/Plugin/Layout/YSLayoutOptions.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\ys_layouts\Plugin\Layout;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Layout\LayoutDefault;
+
+/**
+ * Configuration per section for YS Layouts.
+ */
+class YSLayoutOptions extends LayoutDefault {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $configuration = parent::defaultConfiguration();
+
+    return $configuration + [
+      'divider' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['divider'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Divider'),
+      '#default_value' => $this->configuration['divider'],
+      '#description' => $this->t('Add a divider between the columns.'),
+      '#weight' => 10,
+    ];
+
+    return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    $this->configuration['divider'] = $form_state->getValue('divider') ? 'true' : 'false';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(array $regions): array {
+    $build = parent::build($regions);
+    //kint($build);
+    //$build['divider'] = $this->configuration['divider'];
+
+    return $build;
+  }
+
+}
