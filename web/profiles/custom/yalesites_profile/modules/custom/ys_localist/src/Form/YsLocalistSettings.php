ac10496855c76443637591518f87826fc6f18520
David Blankenship <david.blankenship@yale.edu>
2024-12-10 08:50:40 -0500

    chore(YSP-744): rename class to have YS prefix

---
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_localist/src/Form/YsLocalistSettings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\ys_localist\Form;
+
+use Drupal\localist_drupal\Form\LocalistSettings;
+
+/**
+ * Defines the manage Localist settings interface.
+ */
+class YsLocalistSettings extends LocalistSettings {
+}
