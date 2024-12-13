8e6597daf5581bfb11de306bc070444b1f70ef4e
David Blankenship <david.blankenship@yale.edu>
2024-12-13 10:10:08 -0500

    feat(YSP-770): add config

---
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_campus_groups/src/CampusGroupsConfig.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\ys_campus_groups;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Config class for managing campus group configurations.
+ *
+ * This service provides a way to access and manage the configuration
+ * settings for the "ys_campus_group" module.
+ */
+class CampusGroupsConfig implements ContainerInjectionInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs the CampusGroupService object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory->getEditable('ys_campus_group.settings');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+    );
+  }
+
+  /**
+   * Gets the configuration for ys_campus_group.
+   *
+   * @return \Drupal\Core\Config\ImmutableConfig
+   *   The configuration object.
+   */
+  public function getConfig() {
+    return $this->configFactory;
+  }
+
+}
