5c1ec1e59f327ae81c78466de6019c3dd2d017e6
David Blankenship <david.blankenship@yale.edu>
2025-01-09 14:33:00 -0500

    feat(YSP-770): pluralize campus groups

--- web/profiles/custom/yalesites_profile/modules/custom/ys_campus_groups/src/Form/CampusGroupsSettings.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_campus_groups/src/Form/CampusGroupsSettings.php
@@ -14,55 +14,58 @@ class CampusGroupsSettings extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'ys_campus_group_settings';
+    return 'ys_campus_groups_settings';
   }
 
   /**
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return ['ys_campus_group.settings'];
+    return ['ys_campus_groups.settings'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('ys_campus_group.settings');
+    $config = $this->config('ys_campus_groups.settings');
 
-    $form['enable_campus_group_sync'] = [
+    if ($config->get('enable_campus_groups_sync')) {
+      $form['sync_now_button'] = [
+        '#type' => 'markup',
+        '#markup' => '<a class="button" href="/admin/yalesites/campus_groups/sync">Sync now</a>',
+      ];
+    }
+
+    $form['enable_campus_groups_sync'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Enable Campus Group sync'),
       '#description' => $this->t('Once enabled, Campus Group data will sync events for the selected group roughly every hour.'),
-      '#default_value' => $config->get('enable_campus_group_sync') ?: FALSE,
+      '#default_value' => $config->get('enable_campus_groups_sync') ?: FALSE,
     ];
 
-    $form['campus_group_endpoint'] = [
+    $form['campus_groups_endpoint'] = [
       '#type' => 'url',
       '#required' => TRUE,
       '#title' => $this->t('Campus Group endpoint base URL'),
-      '#description' => $this->t('Ex: https://yaleconnect.yale.edu'),
-      '#default_value' => $config->get('campus_group_endpoint') ?: 'https://yaleconnect.yale.edu',
+      '#description' => $this->t('Ex: https://yaleconnect.yale.edu/rss_events'),
+      '#default_value' => $config->get('campus_groups_endpoint') ?: 'https://yaleconnect.yale.edu/rss_events',
     ];
-    $form['campus_group_future_days'] = [
+    $form['campus_groups_future_days'] = [
       '#type' => 'textfield',
       '#required' => TRUE,
       '#title' => $this->t('Future number of days'),
-      '#default_value' => $config->get('campus_group_future_days') ?: 120,
+      '#default_value' => $config->get('campus_groups_future_days') ?: 120,
       '#description' => $this->t('Enter the number of future days'),
     ];
-    $form['campus_group_groupids'] = [
+    $form['campus_groups_groupids'] = [
       '#type' => 'textfield',
       '#required' => TRUE,
       '#title' => $this->t('Group ids'),
-      '#default_value' => $config->get('campus_group_groupids'),
-      '#description' => $this->t('Enter the group ids comman seperated'),
+      '#default_value' => $config->get('campus_groups_groupids'),
+      '#description' => $this->t('Enter the group ids comma seperated'),
     ];
-    $form['enable_campus_group_redirect'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Automatically redirect users to Campus Groups when they click an event card'),
-      '#default_value' => $config->get('enable_campus_group_redirect') ?: FALSE,
-    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -71,13 +74,11 @@ class CampusGroupsSettings extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
-    $this->configFactory->getEditable('ys_campus_group.settings')
-       // Set the submitted configuration setting.
-      ->set('enable_campus_group_sync', $form_state->getValue('enable_campus_group_sync'))
-      ->set('campus_group_endpoint', rtrim($form_state->getValue('campus_group_endpoint'), "/"))
-      ->set('campus_group_future_days', $form_state->getValue('campus_group_future_days'))
-      ->set('campus_group_groupids', $form_state->getValue('campus_group_groupids'))
-      ->set('enable_campus_group_redirect', $form_state->getValue('enable_campus_group_redirect'))
+    $this->configFactory->getEditable('ys_campus_groups.settings')
+      ->set('enable_campus_groups_sync', $form_state->getValue('enable_campus_groups_sync'))
+      ->set('campus_groups_endpoint', rtrim($form_state->getValue('campus_groups_endpoint'), "/"))
+      ->set('campus_groups_future_days', $form_state->getValue('campus_groups_future_days'))
+      ->set('campus_groups_groupids', $form_state->getValue('campus_groups_groupids'))
       ->save();
 
     parent::submitForm($form, $form_state);
