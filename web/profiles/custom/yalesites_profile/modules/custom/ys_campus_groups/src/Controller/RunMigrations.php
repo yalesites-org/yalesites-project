0ca83a6f2f692f51999ca3c7da15d1e6c86bd35c
David Blankenship <david.blankenship@yale.edu>
2025-01-10 15:05:53 -0500

    feat(YSP-770): create manager for CampusGroups

--- web/profiles/custom/yalesites_profile/modules/custom/ys_campus_groups/src/Controller/RunMigrations.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_campus_groups/src/Controller/RunMigrations.php
@@ -2,17 +2,12 @@
 
 namespace Drupal\ys_campus_groups\Controller;
 
-use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\Extension\ModuleHandler;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Url;
-use Drupal\migrate\MigrateExecutable;
-use Drupal\migrate\MigrateMessage;
-use Drupal\migrate\Plugin\MigrationInterface;
-use Drupal\migrate\Plugin\MigrationPluginManager;
+use Drupal\ys_campus_groups\CampusGroupsManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -21,14 +16,19 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
  */
 class RunMigrations extends ControllerBase implements ContainerInjectionInterface {
 
-  const MIGRATION_NAME = 'campus_groups_events';
-
   /**
    * Configuration Factory.
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
   protected $campusGroupsConfig;
+
+  /**
+   * The Campus Groups Manager.
+   *
+   * @var \Drupal\ys_campus_groups\CampusGroupsManager
+   */
+  protected $campusGroupsManager;
 
   /**
    * Drupal messenger.
@@ -63,16 +63,12 @@ class RunMigrations extends ControllerBase implements ContainerInjectionInterfac
    */
   public function __construct(
     ConfigFactoryInterface $config_factory,
-    MigrationPluginManager $migration_manager,
+    CampusGroupsManager $campus_groups_manager,
     MessengerInterface $messenger,
-    TimeInterface $time,
-    ModuleHandler $module_handler,
   ) {
     $this->campusGroupsConfig = $config_factory->get('ys_campus_groups.settings');
-    $this->migrationManager = $migration_manager;
+    $this->campusGroupsManager = $campus_groups_manager;
     $this->messenger = $messenger;
-    $this->time = $time;
-    $this->moduleHandler = $module_handler;
   }
 
   /**
@@ -81,10 +77,8 @@ class RunMigrations extends ControllerBase implements ContainerInjectionInterfac
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
-      $container->get('plugin.manager.migration'),
+      $container->get('ys_campus_groups.manager'),
       $container->get('messenger'),
-      $container->get('datetime.time'),
-      $container->get('module_handler'),
     );
   }
 
@@ -92,82 +86,22 @@ class RunMigrations extends ControllerBase implements ContainerInjectionInterfac
    * Runs all Localist migrations.
    */
   public function runAllMigrations() {
-    $migration = self::MIGRATION_NAME;
-
     if ($this->campusGroupsConfig->get('enable_campus_groups_sync')) {
-      $this->runMigration($migration);
-      $imported = $this->getMigrationStatus($migration);
-
-      $message = "Synchronized $imported events.";
-      $this->messenger()->addStatus($message);
+      $messageData = $this->campusGroupsManager->runAllMigrations();
+      if ($messageData) {
+        $eventsImported = $messageData['campus_groups_events']['imported'];
+        $message = "Synchronized $eventsImported events.";
+        $this->messenger()->addStatus($message);
+      }
     }
     else {
-      $this->messenger()->addError('Campus Groups syncing is not enabled.');
+      $message = 'Campus Groups syncing is not enabled.';
+      $this->messenger()->addError($message);
     }
 
     $redirectUrl = Url::fromRoute('ys_campus_groups.settings')->toString();
     $response = new RedirectResponse($redirectUrl);
     return $response;
-  }
-
-  /**
-   * Runs specific migration.
-   *
-   * @param string $migration
-   *   The migration ID.
-   *
-   * @return void
-   *   Runs the migration.
-   */
-  public function runMigration($migration) {
-    // Loop over the list of the migrations and check if they require
-    // execution.
-    // Prevent non-existent migrations from breaking cron.
-    $migrationInstance = $this->migrationManager->createInstance($migration);
-    if ($migrationInstance) {
-      // Check if the migration status is IDLE, if not, make it so.
-      $status = $migrationInstance->getStatus();
-      if ($status !== MigrationInterface::STATUS_IDLE) {
-        $migrationInstance->setStatus(MigrationInterface::STATUS_IDLE);
-      }
-
-      /*
-       * @todo Possibly implement the following flags, if needed.
-       * Runs migration with the --update flag.
-       * $migration_update = $migration->getIdMap();
-       * $migration_update->prepareUpdate();
-       * Runs migration with the --sync flag.
-       * The problem here is if editor adds layout builder, this will wipe those
-       * changes out before recreating. So, this not be a good idea.
-       * $migrationInstance->set('syncSource', TRUE);
-       */
-
-      $message = new MigrateMessage();
-      $executable = new MigrateExecutable($migrationInstance, $message);
-      $executable->import();
-
-      /* If using migrate_plus module, update the migrate_last_imported value
-       * for the migration.
-       */
-
-      if ($this->moduleHandler->moduleExists('migrate_plus')) {
-        $migrate_last_imported_store = $this->keyValue('migrate_last_imported');
-        $migrate_last_imported_store->set($migrationInstance->id(), round($this->time->getCurrentMicroTime() * 1000));
-      }
-    }
-  }
-
-  /**
-   * Gets the migration status such as number of items imported.
-   *
-   * @return int
-   *   For now, just the number of items imported.
-   */
-  public function getMigrationStatus($migration_id) {
-    $migration = $this->migrationManager->createInstance($migration_id);
-    $map = $migration->getIdMap();
-    $imported = $map->importedCount();
-    return $imported;
   }
 
 }
