32a8afdf8966369283a5e673f2ae065055cd3095
Marc Berger <marc@fourkitchens.com>
2025-01-31 13:15:47 -0800

    feat(IYY-290): WIP Initial work on anonymous access to menu links

---
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/Menu/YsNodeAccessLinkTreeManipulator.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\ys_node_access\Menu;
+
+use Drupal\Core\Access\AccessManagerInterface;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Menu\DefaultMenuLinkTreeManipulators;
+use Drupal\Core\Menu\MenuLinkInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Custom menu link tree manipulator to override access checks.
+ */
+class YsNodeAccessLinkTreeManipulator extends DefaultMenuLinkTreeManipulators {
+
+  /**
+   * Constructs a CustomMenuLinkTreeManipulator object.
+   *
+   * @param \Drupal\Core\Access\AccessManagerInterface $access_manager
+   *   The access manager.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The current user.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface|null $module_handler
+   *   The module handler.
+   */
+  public function __construct(
+    AccessManagerInterface $access_manager,
+    AccountInterface $account,
+    EntityTypeManagerInterface $entity_type_manager,
+    ModuleHandlerInterface $module_handler = NULL
+  ) {
+    parent::__construct($access_manager, $account, $entity_type_manager, $module_handler);
+  }
+
+  /**
+   * Override the menu link access check.
+   *
+   * @param \Drupal\Core\Menu\MenuLinkInterface $instance
+   *   The menu link instance.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   The access result.
+   */
+  protected function menuLinkCheckAccess(MenuLinkInterface $instance) {
+    $menusToCheck = [
+      'main',
+      'utility-navigation',
+      'utility-drop-button-navigation',
+      'footer',
+    ];
+
+    $menuName = $instance->getMenuName();
+    if (in_array($menuName, $menusToCheck)) {
+      dpm($menuName);
+    }
+
+    $access_result = parent::menuLinkCheckAccess($instance);
+
+    // If the user is anonymous, override access to always allow visibility.
+    // if ($this->account->isAnonymous()) {
+    //   return AccessResult::allowed();
+    // }
+
+    return $access_result;
+  }
+
+}
