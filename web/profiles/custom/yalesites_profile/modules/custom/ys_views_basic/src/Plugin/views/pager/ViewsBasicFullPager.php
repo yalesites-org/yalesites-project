7849a903f7e061933d2ee53f64b7ae85924248d8
David Blankenship <david.blankenship@yale.edu>
2025-03-11 14:44:14 -0400

    refactor(YSP-844): refactor query method for clarity
    Refactored the `query` method in `ViewsBasicFullPager` to improve code
    clarity and maintainability. Introduced helper methods `hasItemsPerPage`,
    `itemsPerPage`, `offset`, `hasTotalPages`, and `pastLastPage` to encapsulate
    specific checks and calculations. Updated method signatures to include
    return types.

--- web/profiles/custom/yalesites_profile/modules/custom/ys_views_basic/src/Plugin/views/pager/ViewsBasicFullPager.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_views_basic/src/Plugin/views/pager/ViewsBasicFullPager.php
@@ -23,23 +23,76 @@ class ViewsBasicFullPager extends Full {
   /**
    * {@inheritdoc}
    */
-  public function query() {
-    if (!isset($this->view->args[5])) {
+  public function query(): void {
+    if (!$this->hasItemsPerPage()) {
       return;
     }
-    $this->setItemsPerPage((int) $this->view->args[5]);
-    $this->setOffset($this->view->args[7]);
+    $this->setItemsPerPage($this->itemsPerPage());
+    $this->setOffset($this->offset());
     $limit = $this->options['items_per_page'];
     $offset = $this->current_page * $this->options['items_per_page'] + $this->options['offset'];
-    if (!empty($this->options['total_pages'])) {
-      if ($this->current_page >= $this->options['total_pages']) {
-        $limit = $this->options['items_per_page'];
-        $offset = $this->options['total_pages'] * $this->options['items_per_page'];
-      }
+    if ($this->hasTotalPages() && $this->pastLastPage()) {
+      // Set them within the bounds of the total pages.
+      $limit = $this->options['items_per_page'];
+      $offset = $this->options['total_pages'] * $this->options['items_per_page'];
     }
 
     $this->view->query->setLimit($limit);
     $this->view->query->setOffset($offset);
+  }
+
+  /**
+   * Checks if the current page is past the last page.
+   *
+   * @return bool
+   *   TRUE if the current page is past the last page, FALSE otherwise.
+   */
+  protected function pastLastPage(): bool {
+    return $this->current_page >= $this->options['total_pages'];
+  }
+
+  /**
+   * Checks if the view has items per page argument.
+   *
+   * @return bool
+   *   TRUE if items per page argument is set, FALSE otherwise.
+   */
+  protected function hasItemsPerPage(): bool {
+    return isset($this->view->args[5]);
+
+  }
+
+  /**
+   * Checks if the total pages option is set.
+   *
+   * @return bool
+   *   TRUE if total pages option is set, FALSE otherwise.
+   */
+  protected function hasTotalPages(): bool {
+    return (!empty($this->options['total_pages']));
+
+  }
+
+  /**
+   * Gets the number of items per page.
+   *
+   * @return int
+   *   The number of items per page.
+   */
+  protected function itemsPerPage(): int {
+    return (int) $this->view->args[5];
+
+  }
+
+  /**
+   * Gets the offset for the query.
+   *
+   * @return int
+   *   The offset for the query.
+   */
+  protected function offset(): int {
+    return (int) $this->view->args[7];
+
   }
 
 }
