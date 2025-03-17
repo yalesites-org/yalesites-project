872426e4d62371f17cc795342e23ea35aa138d8f
David Blankenship <david.blankenship@yale.edu>
2025-03-17 12:52:49 -0400

    feat(YSP-802): enhance integrations block functionality
    - Added action with "Configure" link in the admin menu block.
    - Added conditional "Sync now" action based on module configuration.
    - Introduced helper methods to manage module names and settings:
      - `isTurnedOn`: Checks if sync is enabled for a module.
      - `getModuleNameFromRouteName`: Extracts module name from route.
      - `removeYsPrefix`: Removes "ys_" prefix from module names.
    - Removed debug statement `{{kint()}}` from the Twig template.

--- web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Controller/IntegrationsController.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Controller/IntegrationsController.php
@@ -17,15 +17,59 @@ class IntegrationsController extends SystemController {
     $output = parent::systemAdminMenuBlockPage();
 
     foreach ($output['#content'] as $key => $value) {
-      $output['#content'][$key]['#actions']['sync'] = [
+      $output['#content'][$key]['#actions']['configure'] = [
         '#type' => 'link',
-        '#title' => t('Sync'),
-        '#url' => Url::fromRoute('ys_core.admin_yalesites_integrations'),
+        '#title' => t('Configure'),
+        '#url' => $output['#content'][$key]['url'],
+        '#options' => [
+          'attributes' => [
+            'class' => ['button', 'button--primary'],
+          ],
+        ],
       ];
+
+      $module_name = $this->getModuleNameFromRouteName($output['#content'][$key]['url']->getRouteName());
+      if ($this->isTurnedOn($module_name)) {
+        $output['#content'][$key]['#actions']['sync'] = [
+          '#type' => 'link',
+          '#title' => t('Sync now'),
+          '#url' => Url::fromRoute($module_name . 'run_migrations'),
+          '#options' => [
+            'attributes' => [
+              'class' => ['button', 'button--primary'],
+            ],
+          ],
+        ];
+      }
     }
 
     $output['#theme'] = 'ys_integrations_block';
     return $output;
+
+  }
+
+  /**
+   *
+   */
+  protected function isTurnedOn($module_name) {
+    $config = \Drupal::config($module_name . '.settings');
+    $settings_name = $this->removeYsPrefix($module_name);
+    return $config->get('enable_' . $settings_name . '_sync');
+  }
+
+  /**
+   *
+   */
+  protected function getModuleNameFromRouteName($route_name) {
+    $route_parts = explode('.', $route_name);
+    return $route_parts[0];
+  }
+
+  /**
+   *
+   */
+  protected function removeYsPrefix($name) {
+    return str_replace('ys_', '', $name);
   }
 
 }
