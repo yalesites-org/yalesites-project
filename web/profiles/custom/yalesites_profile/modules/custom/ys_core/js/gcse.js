46393843960735246d8d108fc7485b6d90fdbed7
Duncan <8762630+duncancm9@users.noreply.github.com>
2025-04-21 15:32:37 -0400

    feat: add google programmable search js

---
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_core/js/gcse.js
@@ -0,0 +1,18 @@
+(function ($) {
+  Drupal.behaviors.gcseSearchTab = {
+    attach: function (context, settings) {
+      gcseLoad();
+
+      function gcseLoad() {
+        var cx = '013786538304926843299:7eimwqn6viu';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+        window.gcseLoaded = true;
+      }
+    }
+  };
+})(jQuery);
