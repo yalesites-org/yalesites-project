00b539523595ab552cd7db3bf77bb9c339b458ef
Vincent Massaro <vincent.massaro@yale.edu>
2025-06-09 17:04:03 -0400

    feat: upgrade to drupal/single_content_sync:1.4.13 Also updates from Simplefield to GenericField due to change in https://www.drupal.org/project/single_content_sync/issues/3498506.

--- web/profiles/custom/yalesites_profile/modules/custom/ys_starterkit/src/Plugin/SingleContentSyncFieldProcessor/ViewsBasicParams.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_starterkit/src/Plugin/SingleContentSyncFieldProcessor/ViewsBasicParams.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\ys_starterkit\Plugin\SingleContentSyncFieldProcessor;
 
-use Drupal\single_content_sync\Plugin\SingleContentSyncFieldProcessor\SimpleField;
+use Drupal\single_content_sync\Plugin\SingleContentSyncFieldProcessor\GenericField;
 
 /**
  * Single Content Sync ViewsBasicParams Handler.
@@ -13,5 +13,5 @@ use Drupal\single_content_sync\Plugin\SingleContentSyncFieldProcessor\SimpleFiel
  *   field_type = "views_basic_params",
  * )
  */
-class ViewsBasicParams extends SimpleField {
+class ViewsBasicParams extends GenericField {
 }
