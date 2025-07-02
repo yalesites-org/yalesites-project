8e6044d2dad6866690c62096f99958f4f84fb9dd
Steven Keiser <steven.keiser@square360.com>
2025-07-02 14:53:15 -0400

    feat(YSE-106): update resource meta block

--- web/profiles/custom/yalesites_profile/modules/custom/ys_layouts/src/Plugin/Block/ResourceMetaBlock.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_layouts/src/Plugin/Block/ResourceMetaBlock.php
@@ -125,33 +125,88 @@ class ResourceMetaBlock extends BlockBase implements ContainerFactoryPluginInter
     }
 
     $title = NULL;
-    $publishDate = NULL;
-    $dateFormatted = NULL;
-    $category = NULL;
+    $categoryName = NULL;
+    $publishedOn = NULL;
+    $metadata = [];
+    $mediaBundle = NULL;
+    $fileUrl = NULL;
+    $mediaId = NULL;
 
     $route = $this->routeMatch->getRouteObject();
 
     if ($route) {
-      // Resource fields.
+      // Get Resource fields.
       $title = $node->getTitle();
-      $publishDate = strtotime($node->field_publish_date->first()->getValue()['value']);
-      $dateFormatted = $this->dateFormatter->format($publishDate, '', 'c');
-      // $media = $node->field_media->first()->getValue();
-      $category = $node->field_category->first()->getValue();
+      $fieldPublishDate = $node->field_publish_date;
+      $fieldCategory = $node->field_category->first()->getValue();
+      $fieldMedia = $node->field_media?->first()?->getValue();
 
-      if ($category) {
-        /** @var \Drupal\taxonomy\Entity\Term $categoryInfo */
-        $categoryInfo = $this->entityTypeManager->getStorage('taxonomy_term')->load($category['target_id']);
+      // Set the "published on" date.
+      $publishDateValue = strtotime($fieldPublishDate->first()->getValue()['value']);
+      $publishDateLabel = $fieldPublishDate->getFieldDefinition()->getLabel();
+      $dateFormatted = $this->dateFormatter->format($publishDateValue, '', 'F j, Y');
+      $publishedOn = "$publishDateLabel: $dateFormatted";
 
-        $category = $categoryInfo->getName();
+      // Get Category term.
+      /** @var \Drupal\taxonomy\Entity\Term $categoryTerm */
+      $categoryTerm = $this->entityTypeManager->getStorage('taxonomy_term')->load($fieldCategory['target_id']);
+      $categoryName = $categoryTerm->getName();
+
+      // Set metadata.
+      $selected_term_fields = [
+        'field_custom_vocab',
+        'field_audience',
+      ];
+
+      foreach ($selected_term_fields as $field_name) {
+        if ($node->hasField($field_name)) {
+          $field = $node->get($field_name);
+          $field_label = $field->getFieldDefinition()->getLabel();
+          $terms = [];
+
+          foreach ($field->referencedEntities() as $term) {
+            $terms[] = [
+              '#type' => 'link',
+              '#title' => $term->label(),
+              '#url' => $term->toUrl(),
+            ];
+          }
+
+          $metadata[$field_name] = [
+            'label' => $field_label,
+            'items' => $terms,
+          ];
+        }
+      }
+
+      // Set media.
+      if ($fieldMedia) {
+        /** @var \Drupal\media\Entity\Media $media */
+        $media = $this->entityTypeManager->getStorage('media')->load($fieldMedia['target_id']);
+        $mediaId = $media->id();
+        $mediaBundle = $media->bundle();
+        $mediaLabel = $media->label();
+
+        if ($mediaBundle === 'document') {
+          $fieldMediaFile = $media->field_media_file->first()->getValue();
+
+          /** @var \Drupal\file\Entity\File $file */
+          $file = $this->entityTypeManager->getStorage('file')->load($fieldMediaFile['target_id']);
+          $fileUrl = $file->createFileUrl();
+        }
       }
     }
 
     return [
       '#theme' => 'ys_resource_meta_block',
       '#resource_meta__heading' => $title,
-      '#resource_meta__date_formatted' => $dateFormatted,
-      '#resource_meta__category' => $category,
+      '#resource_meta__published_on' => $publishedOn,
+      '#resource_meta__category' => $categoryName,
+      '#resource_meta__metadata' => $metadata,
+      '#resource_meta__resource_type' => $mediaBundle,
+      '#resource_meta__download_url' => $fileUrl,
+      '#resource_meta__download_label' => $this->t('Download') . ' ' . $mediaLabel,
+      '#resource_meta__media_id' => $mediaId,
     ];
   }
 
