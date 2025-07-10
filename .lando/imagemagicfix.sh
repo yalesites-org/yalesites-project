8df223dcb4e61bc335fe85f6145b69d80b9c5f41
George A Heimel <george.heimel@square360.com>
2025-07-10 11:43:44 -0400

    feat(YSE-106): Added lando configs so that imagemagick will make thumbnails
    This fixes an issue with the Lando Pantheon recipes so that the Thumbnail Media PDF module can work with ImageMagick 6.9 that Pantheon has spec’d in their docker containers

---
+++ .lando/imagemagicfix.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "Fixing ImageMagick for PDF Thumbnails"
+sed -i 's/<policy domain="coder" rights="none" pattern="PDF" \/>/<policy domain="coder" rights="read|write" pattern="PDF" \/>/g' /etc/ImageMagick-6/policy.xml
