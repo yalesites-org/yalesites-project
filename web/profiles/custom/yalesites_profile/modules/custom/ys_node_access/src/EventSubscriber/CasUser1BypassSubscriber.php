c1dfeab6b7b6acc047c098cfe7c886c7db32af2e
David Blankenship <david.blankenship@yale.edu>
2025-07-11 10:41:51 -0400

    fix(YSP-986): remove unused variable in CasUser1BypassSubscriber

--- web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/EventSubscriber/CasUser1BypassSubscriber.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/EventSubscriber/CasUser1BypassSubscriber.php
@@ -67,7 +67,6 @@ class CasUser1BypassSubscriber implements EventSubscriberInterface {
       return;
     }
 
-    $request = $event->getRequest();
     $config = $this->configFactory->getEditable('cas.settings');
 
     // Check if forced login is enabled.
