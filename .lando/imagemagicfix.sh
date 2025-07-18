01c44ada19247fc5ac83fd117c3e06767d4e52c3
George A Heimel <george.heimel@square360.com>
2025-07-18 10:06:38 -0400

    fix(YSE-106): fixed imagick php module scripts
    - changed the imagemagick script to use “build_as_root” lando parameter
    - changed how the script is getting the src and compiling the php extension

--- .lando/imagemagicfix.sh
+++ .lando/imagemagicfix.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
-# echo "Looking for ImageMagick php extension"
-# if [ -f "/usr/src/php/ext/imagick" ]; then
-#   echo "imagick src folder exists";
-# else
-#   echo "imagick src folder does not exist, creating it";
-#   mkdir -p /usr/src/php/ext/imagick
-#   curl -fsSL https://github.com/Imagick/imagick/archive/28f27044e435a2b203e32675e942eb8de620ee58.tar.gz | tar xvz -C "/usr/src/php/ext/imagick" --strip 1
-#   docker-php-ext-install imagick
-#   composer update
-# fi
+echo "Looking for ImageMagick php extension"
+if [ php -m | grep imagick ]; then
+  echo "imagick src folder exists";
+else
+  echo "==================================================================================================="
+  echo "Fixing ImageMagick for PDF Thumbnails"
+  git clone https://github.com/Imagick/imagick.git --depth 1 /usr/src/php/ext/imagick && \
+  cd /usr/src/php/ext/imagick && \
+  git fetch origin master && \
+  git switch master && \
+  phpize && \
+  ./configure && \
+  make && \
+  make install && \
+  docker-php-ext-enable imagick && \
+  sed -i 's#<policy domain="coder" rights="none" pattern="PDF" />#<policy domain="coder" rights="read|write" pattern="PDF" />#g' /etc/ImageMagick-6/policy.xml
+  echo "ImageMagick fix complete"
+  echo "==================================================================================================="
+fi
 
-# echo "Fixing ImageMagick for PDF Thumbnails"
-# sed -i 's/<policy domain="coder" rights="none" pattern="PDF" \/>/<policy domain="coder" rights="read|write" pattern="PDF" \/>/g' /etc/ImageMagick-6/policy.xml
-
-echo "Fixing ImageMagick for PDF Thumbnails"
-tar -xzf /usr/src/php.tar.xz
-mkdir -p /usr/src/php/ext/imagick
-curl -fsSL https://github.com/Imagick/imagick/archive/28f27044e435a2b203e32675e942eb8de620ee58.tar.gz | tar xvz -C "/usr/src/php/ext/imagick" --strip 1
-docker-php-ext-install imagick
-sed -i 's/<policy domain="coder" rights="none" pattern="PDF" \/>/<policy domain="coder" rights="read | write" pattern="PDF" \/>/g' /etc/ImageMagick-6/policy.xml
-composer install
-echo "ImageMagick fix complete"
