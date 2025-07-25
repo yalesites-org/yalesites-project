ea8749fc5ada194dd53e77deadf9a5562c7d6aff
David Blankenship <david.blankenship@yale.edu>
2025-07-25 05:53:30 -0400

    style(IYY-360): remove now identical constructor

--- web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/Menu/YsNodeAccessLinkTreeManipulator.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/Menu/YsNodeAccessLinkTreeManipulator.php
@@ -2,40 +2,15 @@
 
 namespace Drupal\ys_node_access\Menu;
 
-use Drupal\Core\Access\AccessManagerInterface;
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Menu\DefaultMenuLinkTreeManipulators;
 use Drupal\Core\Menu\MenuLinkInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\node\NodeInterface;
 
 /**
  * Custom menu link tree manipulator to override access checks.
  */
 class YsNodeAccessLinkTreeManipulator extends DefaultMenuLinkTreeManipulators {
-
-  /**
-   * Constructs a CustomMenuLinkTreeManipulator object.
-   *
-   * @param \Drupal\Core\Access\AccessManagerInterface $access_manager
-   *   The access manager.
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The current user.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface|null $module_handler
-   *   The module handler.
-   */
-  public function __construct(
-    AccessManagerInterface $access_manager,
-    AccountInterface $account,
-    EntityTypeManagerInterface $entity_type_manager,
-    ModuleHandlerInterface $module_handler,
-  ) {
-    parent::__construct($access_manager, $account, $entity_type_manager, $module_handler);
-  }
 
   /**
    * Override the menu link access check.
