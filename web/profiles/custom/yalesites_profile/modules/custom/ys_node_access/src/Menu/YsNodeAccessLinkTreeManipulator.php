d86530753b626f7c0c2de33cf53aa5159cbffc97
David Blankenship <david.blankenship@yale.edu>
2025-10-20 12:03:36 -0400

    feat(node-access): optimize CAS restriction check for menu links
    Refactor YsNodeAccessLinkTreeManipulator to use direct database queries
    for CAS restriction checks instead of loading full node entities. This
    improves performance by caching results and reduces unnecessary entity
    loads. Update service definition to inject the database connection.
    Removes the isCasRestricted method and related NodeInterface usage.

--- web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/Menu/YsNodeAccessLinkTreeManipulator.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_node_access/src/Menu/YsNodeAccessLinkTreeManipulator.php
@@ -3,14 +3,29 @@
 namespace Drupal\ys_node_access\Menu;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Menu\DefaultMenuLinkTreeManipulators;
 use Drupal\Core\Menu\MenuLinkInterface;
-use Drupal\node\NodeInterface;
 
 /**
  * Custom menu link tree manipulator to override access checks.
  */
 class YsNodeAccessLinkTreeManipulator extends DefaultMenuLinkTreeManipulators {
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($access_manager, $account, $entity_type_manager, $module_handler, Connection $database) {
+    parent::__construct($access_manager, $account, $entity_type_manager, $module_handler);
+    $this->database = $database;
+  }
 
   /**
    * Override the menu link access check.
@@ -22,9 +37,8 @@ class YsNodeAccessLinkTreeManipulator extends DefaultMenuLinkTreeManipulators {
    *   The access result.
    */
   protected function menuLinkCheckAccess(MenuLinkInterface $instance) {
-    /*
-     * Allows anonymous users to see CAS only links.
-     */
+    static $casRestrictedNodes = [];
+
     $access_result = parent::menuLinkCheckAccess($instance);
 
     $menuName = $instance->getMenuName();
@@ -32,13 +46,21 @@ class YsNodeAccessLinkTreeManipulator extends DefaultMenuLinkTreeManipulators {
       $nodeId = $instance->getRouteParameters()['node'] ?? NULL;
 
       if ($nodeId) {
-        $node = $this->entityTypeManager->getStorage('node')->load($nodeId);
-        if ($node && $this->isCasRestricted($node)) {
+        if (!isset($casRestrictedNodes[$nodeId])) {
+          $query = $this->database->select('node__field_login_required', 'lr')
+            ->fields('lr', ['field_login_required_value'])
+            ->condition('lr.entity_id', $nodeId)
+            ->condition('lr.deleted', 0)
+            ->execute();
+
+          $casRestrictedNodes[$nodeId] = ($query->fetchField() == 1);
+        }
+
+        if ($casRestrictedNodes[$nodeId]) {
           $metadata = $instance->getMetaData();
           $menu_link_content_storage = $this->entityTypeManager->getStorage('menu_link_content');
           $menu_entity = $menu_link_content_storage->load($metadata['entity_id']);
 
-          // Adds a property to be read by ys_node_access.module for styling.
           $menu_entity->data_restricted = TRUE;
           return AccessResult::allowed();
         }
@@ -46,22 +68,6 @@ class YsNodeAccessLinkTreeManipulator extends DefaultMenuLinkTreeManipulators {
     }
 
     return $access_result;
-  }
-
-  /**
-   * Checks if the node is CAS restricted.
-   *
-   * @param \Drupal\node\NodeInterface $node
-   *   The node to check.
-   *
-   * @return bool
-   *   True if the node is CAS restricted, false otherwise.
-   */
-  protected function isCasRestricted(NodeInterface $node) {
-    return (
-      $node->hasField('field_login_required') &&
-      $node->get('field_login_required')->value == 1
-    );
   }
 
 }
