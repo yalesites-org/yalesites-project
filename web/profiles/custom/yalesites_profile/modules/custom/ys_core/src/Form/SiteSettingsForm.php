a0f8b0e13d5d11820f5b0aed26bbcb85eeb35296
laura-johnson <laura@fourkitchens.com>
2026-03-09 09:13:35 -0400

    1094 remove the required attribute

--- web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Form/SiteSettingsForm.php
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_core/src/Form/SiteSettingsForm.php
@@ -239,7 +239,6 @@ class SiteSettingsForm extends ConfigFormBase implements ContainerInjectionInter
       '#description' => $this->t('This field will update the name of the custom vocabulary for the site. By default, the name is "Custom Vocab".'),
       '#title' => $this->t('Custom Vocabulary Name'),
       '#default_value' => $yaleConfig->get('taxonomy')['custom_vocab_name'] ?? 'Custom Vocab',
-      '#required' => TRUE,
     ];
 
     $form['font_pairing'] = [
